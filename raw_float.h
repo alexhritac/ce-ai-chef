81a822000dc37ef3a8e853834b5305315828d878
Victor <523337@student.saxion.nl>
2022-12-01 13:15:29 +0100

    Increasing the functionality

--- raw_float.h
+++ raw_float.h
@@ -8,21 +8,39 @@
 
 class raw_float {
 public:
+    // Constructor
     explicit raw_float(const std::string& input);
 
+    // Printing function
     [[nodiscard]] std::string to_string() const;
 
-    raw_float& operator+=(const raw_float& second);
-    raw_float& operator-=(const raw_float& second);
-    bool operator==(const raw_float& other)const;
+    // Operators
+    raw_float& operator+=(const raw_float& other);
+    raw_float& operator-=(const raw_float& other);
+    raw_float& operator*=(const raw_float& other);
+    raw_float& operator/=(const raw_float& other);
+    raw_float operator+(const raw_float& other) const;
+    raw_float operator-(const raw_float& other) const;
+    raw_float operator*(const raw_float& other) const;
+    raw_float operator/(const raw_float& other) const;
+
+    // Comparison operators
+    bool operator< (const raw_float& other) const;
+    bool operator<=(const raw_float& other) const;
+    bool operator> (const raw_float& other) const;
+    bool operator>=(const raw_float& other) const;
+    bool operator==(const raw_float& other) const;
     bool operator!=(const raw_float& other) const;
 
-    [[nodiscard]] long long base() const;
-    [[nodiscard]] long long exp() const;
+    // Extra functions
+    raw_float sqrt() const;
+    raw_float to_power(int power) const;
+    // Get functions
+    [[nodiscard]] unsigned long long base() const;
+    [[nodiscard]] bool minus() const;
 private:
-    void check();
-    long long base_;
-    long long exp_;
+    unsigned long long base_;
+    bool minus_;
 };
 
 
